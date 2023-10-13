0a2bd6a8d6862b3c2441f2f21c1436033865effd
elliotallen1 <allened@hendrix.edu>
2023-10-13 15:44:23 -0500

    Added search (by title)
    You can now search for a lego set based on its title

--- RazorPagesLego/Pages/Legos/Index.cshtml.cs
+++ RazorPagesLego/Pages/Legos/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesLego.Data;
 using RazorPagesLego.Models;
@@ -21,12 +22,25 @@ namespace RazorPagesLego.Pages.Legos
 
         public IList<Lego> Lego { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Titles { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? LegoTitle { get; set; }
+
+
         public async Task OnGetAsync()
+    {
+        var legos = from l in _context.Lego
+                     select l;
+        if (!string.IsNullOrEmpty(SearchString))
         {
-            if (_context.Lego != null)
-            {
-                Lego = await _context.Lego.ToListAsync();
-            }
+            legos = legos.Where(s => s.Title.Contains(SearchString));
         }
+
+        Lego = await legos.ToListAsync();
+    }
     }
 }
